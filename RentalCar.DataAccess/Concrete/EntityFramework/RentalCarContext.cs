491e5f4da79f7193c1ba43a239bb148e31a0c345
sakirsuleymanoglu <sakirsuleymanoglu34@gmail.com>
2021-02-21 21:27:06 +0300

    Repository

---
+++ RentalCar.DataAccess/Concrete/EntityFramework/RentalCarContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using RentalCar.Entities.Concrete;
+
+namespace RentalCar.DataAccess.Concrete.EntityFramework
+{
+    public class RentalCarContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-RCE6ARO\SQLEXPRESS; Initial Catalog=RentalCar; Integrated Security=True;");
+        }
+
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Color> Colors { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+    }
+}
