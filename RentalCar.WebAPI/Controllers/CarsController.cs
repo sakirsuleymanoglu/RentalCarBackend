ae9d2e9e99dbb6863a23a2167abc1472645b4bea
sakirsuleymanoglu <sakirsuleymanoglu34@gmail.com>
2021-02-21 22:55:38 +0300

    22:55

---
+++ RentalCar.WebAPI/Controllers/CarsController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RentalCar.Business.Abstract;
+using RentalCar.Entities.Concrete;
+
+namespace RentalCar.WebAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        private ICarService _carService;
+
+        public CarsController(ICarService carService)
+        {
+            _carService = carService;
+        }
+
+        [HttpGet]
+        public IActionResult GetCars()
+        {
+            var result = _carService.GetAll();
+
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+
+            return NotFound(result);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCarById(int id)
+        {
+            var result = _carService.Get(id);
+
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+
+            return NotFound(result);
+        }
+
+        [HttpPost]
+        public IActionResult AddCar(Car car)
+        {
+            var result = _carService.Add(car);
+
+            if (result.Success)
+            {
+                return Created("", result);
+            }
+
+            return BadRequest();
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteCar(Car car)
+        {
+            var result = _carService.Delete(car);
+
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+
+            return NotFound(result);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateCar(Car car)
+        {
+            var result = _carService.Update(car);
+
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+
+            return NotFound(result);
+        }
+
+    }
+}
