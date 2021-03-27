aa13218ba01f1569684899978f9c598547553376
sakirsuleymanoglu <sakirsuleymanoglu34@gmail.com>
2021-03-27 17:17:13 +0300

    cache

--- RentalCar.Core/CrossCuttingCorcerns/Caching/Microsoft/MemoryCacheManager.cs
+++ RentalCar.Core/CrossCuttingCorcerns/Caching/Microsoft/MemoryCacheManager.cs
@@ -3,7 +3,9 @@ using Microsoft.Extensions.DependencyInjection;
 using RentalCar.Core.Utilities.IoC;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace RentalCar.Core.CrossCuttingCorcerns.Caching.Microsoft
 {
@@ -43,7 +45,27 @@ namespace RentalCar.Core.CrossCuttingCorcerns.Caching.Microsoft
 
         public void RemoveByPattern(string pattern)
         {
-            
+            var cacheEntriesCollectionDefinition = typeof(MemoryCache).GetProperty("EntriesCollection", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+
+            var cacheEntriesCollection = cacheEntriesCollectionDefinition.GetValue(_memoryCache) as dynamic;
+
+            List<ICacheEntry> cacheCollectionValues = new List<ICacheEntry>();
+
+            foreach (var cacheItem in cacheEntriesCollection)
+            {
+                ICacheEntry cacheItemValue = cacheItem.GetType().GetProperty("Value").GetValue(cacheItem, null);
+
+                cacheCollectionValues.Add(cacheItemValue);
+            }
+
+            var regex = new Regex(pattern, RegexOptions.Singleline | RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+            var keysToRemove = cacheCollectionValues.Where(d => regex.IsMatch(d.Key.ToString())).Select(d => d.Key).ToList();
+
+            foreach (var key in keysToRemove)
+            {
+                _memoryCache.Remove(key);
+            }
         }
     }
 }
