732d5e4e6018756f0ab227d3500fbfcc40ef0ad9
sakirsuleymanoglu <sakirsuleymanoglu34@gmail.com>
2021-03-31 04:37:18 +0300

    31 mart

---
+++ RentalCar.Business/Utilities/Constants/Messages.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RentalCar.Business.Utilities.Constants
+{
+    public static class Messages
+    {
+        
+    }
+}
